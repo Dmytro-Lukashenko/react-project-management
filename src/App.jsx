5f61de08132c6ff1ccc1038178892e996b8929e3
Dmytro Lukashenko <d.a.lukashenko@gmail.com>
2024-09-09 09:45:52 +0300

    feat: manage creating new project state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import NewProject from "./components/NewProject";
 import ProjectSidebar from "./components/ProjectSidebar";
 import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from 'react';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+  const handleAddNewProject = () => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+  
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onAddProject={handleAddNewProject} />
+  }
+
+
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
-      {/* <NewProject /> */}
+      <ProjectSidebar onAddProject={handleAddNewProject} />
+      {content}
     </main>
   );
 }

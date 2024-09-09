83907aaeef22339e4f14e065c97510121da75ac7
Dmytro Lukashenko <d.a.lukashenko@gmail.com>
2024-09-09 09:14:02 +0300

    feat: add NoProjectSelect, Button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NewProject from "./components/NewProject";
 import ProjectSidebar from "./components/ProjectSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
-      <NewProject />
+      <NoProjectSelected />
+      {/* <NewProject /> */}
     </main>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,8 @@
 const Input = ({textarea, label, ...props}) => {
-    return <p>
-        <label htmlFor="">{label}</label>
-        {textarea ? <textarea {...props}/> : <input type="text" {...props} />}
+    const classes="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+    return <p className="flex flex-col gap-1 my-4">
+        <label className="text-sm font-bold uppercase text-stone-500" htmlFor="">{label}</label>
+        {textarea ? <textarea className={classes} {...props}/> : <input className={classes} type="text" {...props} />}
     </p>
 }
 
